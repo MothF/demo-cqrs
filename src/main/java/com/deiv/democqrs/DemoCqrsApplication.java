2dc0929f6133822a943f11e4bef39a2389643e5a
Denis Ivanov <den.an.iv@gmail.com>
2023-02-01 23:30:11 +0400

    Initial commit

---
+++ src/main/java/com/deiv/democqrs/DemoCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.deiv.democqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCqrsApplication.class, args);
+	}
+
+}
